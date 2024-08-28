a1301a762030839d865f147bcaca2bca136cfdbe
graf27 <graf27@X-KNOY416PC24>
2024-08-28 13:43:03 -0400

    Add project files.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main();
+{
+    float A;
+    float B;
+    float X;
+
+    cout << "Please enter a Value for A: ";
+    cin >> A;
+
+    cout << "A = " << A << endl;
+
+
+    cout << "Please enter a Value for B: ";
+    cin >> B;
+
+
+
+
+
+}
+
+
